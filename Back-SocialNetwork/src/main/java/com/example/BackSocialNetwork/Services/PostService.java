8c6b16fa9eae344294bdb379e0a4ca7b56a9c6b3
LudmilaAl <109355494+LudmilaAl@users.noreply.github.com>
2023-03-20 14:35:49 -0300

    Service - Posts

---
+++ Back-SocialNetwork/src/main/java/com/example/BackSocialNetwork/Services/PostService.java
@@ -0,0 +1,48 @@
+package com.example.BackSocialNetwork.Services;
+
+import com.example.BackSocialNetwork.Entities.Posts;
+import com.example.BackSocialNetwork.Interfaces.PostInterface;
+import com.example.BackSocialNetwork.Repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService implements PostInterface {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Override
+    public List<Posts> getAllPosts() {
+        List<Posts> posts = postRepository.findAll();
+        return posts;
+    }
+
+    @Override
+    public Posts findPost(Long id) {
+        return postRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Posts> getPostsByUserId(Long userId) {
+        return null;
+    }
+
+    @Override
+    public void savePost(Posts posts) {
+        postRepository.save(posts);
+    }
+
+    @Override
+    public void deletePost(Long id) {
+        postRepository.deleteById(id);
+    }
+
+
+
+
+
+
+}
